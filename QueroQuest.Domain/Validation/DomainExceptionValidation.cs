58e70ab9894ee348ba9b343ab5a5c8676ff59d39
Edigleyson Alves <edgleysonalves@gmail.com>
2023-07-02 12:35:03 -0300

    alter file .gitignore

--- QueroQuest.Domain/Validation/DomainExceptionValidation.cs
+++ QueroQuest.Domain/Validation/DomainExceptionValidation.cs
@@ -1,13 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+namespace QueroQuest.Domain.Validation;
 
-namespace QueroQuest.Domain.Validation
+public class DomainExceptionValidation : Exception
 {
-    public class DomainExceptionValidation : Exception
-    {
-        public DomainExceptionValidation(string error) : base(error)
-        { }
-    }
+    public DomainExceptionValidation(string error) : base(error)
+    { }
 }
